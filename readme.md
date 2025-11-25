dbfe68b7afa5b72a295753f16836edf6d6fab233
jwatson <jwatson2396@gmail.com>
2025-11-25 15:30:44 -0500

    starting sudoku in Rust

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Goal
+try and build a sudoku game in Rust
+
+1. how to generate a board to the terminal?
+2. how to generate a solved sudoku board?
+3. how to generate a solvable sudoku game?
+4. how to check if input is correct or not?
+5. how to to user input?
+6. how to show the board?
---
+++ src/main.rs
@@ -0,0 +1,94 @@
+use std::fmt;
+use std::mem;
+
+#[derive(Debug)]
+struct Structure(i32);
+
+impl fmt::Display for Structure {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+// The following struct is for the activity.
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+
+impl fmt::Display for Matrix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {}, {}, {})", self.0, self.1, self.2, self.3)
+    }
+}
+
+// a note: rust has implied returns
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    // `let` can be used to bind the members of a tuple to variables.
+    let (int_param, bool_param) = pair;
+
+    return (bool_param, int_param)
+}
+
+fn transpose(m: Matrix) -> Matrix {
+    let a = m.0;
+    let b = m.1;
+
+    let t = Matrix(b, a, m.2, m.3);
+    return t;
+}
+
+// This function borrows a slice.
+fn analyze_slice(slice: &[i32]) {
+    println!("First element of the slice: {}", slice[0]);
+    println!("The slice has {} elements", slice.len());
+}
+
+// here in the rust by example site, https://doc.rust-lang.org/rust-by-example/primitives/array.html    let s = Structure(1);
+fn main() {
+
+    println!("Hello, world!");
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{}", matrix);
+
+    println!("Matrix: {}", matrix);
+    println!("Transpose:\n{}", transpose(matrix));
+
+    // Fixed-size array (type signature is superfluous).
+    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // All elements can be initialized to the same value.
+    let ys: [i32; 500] = [0; 500];
+
+    // Arrays are stack allocated.
+    println!("Array occupies {} bytes", mem::size_of_val(&xs));
+
+    // Arrays can be automatically borrowed as slices.
+    println!("Borrow the whole array as a slice.");
+    analyze_slice(&xs);
+
+    // Slices can point to a section of an array.
+    // They are of the form [starting_index..ending_index].
+    // `starting_index` is the first position in the slice.
+    // `ending_index` is one more than the last position in the slice.
+    println!("Borrow a section of the array as a slice.");
+    analyze_slice(&ys[1 .. 4]);
+
+    // Example of empty slice `&[]`:
+    let empty_array: [u32; 0] = [];
+    assert_eq!(&empty_array, &[]);
+    assert_eq!(&empty_array, &[][..]); // Same but more verbose
+
+    // Arrays can be safely accessed using `.get`, which returns an
+    // `Option`. This can be matched as shown below, or used with
+    // `.expect()` if you would like the program to exit with a nice
+    // message instead of happily continue.
+    for i in 0..xs.len() + 1 { // Oops, one element too far!
+        match xs.get(i) {
+            Some(xval) => println!("{}: {}", i, xval),
+            None => println!("Slow down! {} is too far!", i),
+        }
+    }
+
+
+}
