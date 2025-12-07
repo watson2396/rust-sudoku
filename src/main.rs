8b7c4e3857bc34c42e2cf1d4f9ff78483ae9ddf1
jwatson <jwatson2396@gmail.com>
2025-12-07 17:17:04 -0500

    update: working on filling a 1d array with random numbers

--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,39 @@
-// An attribute to hide warnings for unused code.
-#![allow(dead_code)]
 
-// use std::fmt;
-// use std::mem;
+use rand::prelude::*;
 
-#[derive(Debug)]
-struct Person {
-    name: String,
-    age: u8,
-}
+// lets try filling an array with any missing numbers from 1-9
+/*
+9 |
+8 |
+7 |
+6 |
+5 |
+4 |
+3 | ...
+2 | a2 b2 c2 d2 e2 f2 h2 i2 j2
+1 | a1 b1 c1 d1 e1 f1 h1 i1 j1
+    --------------------------
+    a  b  c  d  e  f  h  i  j
+*/
 
-// A unit struct
-struct Unit;
-
-// A tuple struct
-struct Pair(i32, f32);
-
-// A struct with two fields
-struct Point {
-    x: f32,
-    y: f32,
-}
-
-// Structs can be reused as fields of another struct
-struct Rectangle {
-    // A rectangle can be specified by where the top left and bottom right
-    // corners are in space.
-    top_left: Point,
-    bottom_right: Point,
-}
-
-fn rect_area (rect: &Rectangle) -> f32 {
-
-    let (&Point { x: x1, y: y1 }, &Point { x: x2, y: y2 }) = (&rect.top_left, &rect.bottom_right);
-
-    return (x1 - x2) * (y1 - y2);
-
-}
-
-// here in the rust by example site, https://doc.rust-lang.org/rust-by-example/custom_types/structs.html
+// here in the rust by example site, https://doc.rust-lang.org/rust-by-example/flow_control/while.html
 fn main() {
 
-    println!("Hello, world!");
+    println!("Hello, World!");
 
-    // Create struct with field init shorthand
-    let name = String::from("Peter");
-    let age = 27;
-    let peter = Person { name, age };
+    // Fixed-size array (type signature is superfluous).
+    let mut a: [i8; 9];
 
-    // Print debug struct
-    println!("{:?}", peter);
+    // Get an RNG:
+    let mut rng = rand::rng();
 
-    // Instantiate a `Point`
-    let point: Point = Point { x: 5.2, y: 0.4 };
-    let another_point: Point = Point { x: 10.3, y: 0.2 };
+    println!("i8: '{}'", rng.random_range(1..10));
+    let mut x = 0;
+    while x < 10 {
+        a[x] = rng.random_range(1..10);
 
-    // Access the fields of the point
-    println!("point coordinates: ({}, {})", point.x, point.y);
+        x += 1;
+    }
 
-    // Make a new point by using struct update syntax to use the fields of our
-    // other one
-    let bottom_right = Point { x: 10.3, ..another_point };
-
-    // `bottom_right.y` will be the same as `another_point.y` because we used that field
-    // from `another_point`
-    println!("second point: ({}, {})", bottom_right.x, bottom_right.y);
-
-    // Destructure the point using a `let` binding
-    let Point { x: left_edge, y: top_edge } = point;
-
-    let _rectangle = Rectangle {
-        // struct instantiation is an expression too
-        top_left: Point { x: left_edge, y: top_edge },
-        bottom_right: bottom_right,
-    };
-
-    // Instantiate a unit struct
-    let _unit = Unit;
-
-    // Instantiate a tuple struct
-    let pair = Pair(1, 0.1);
-
-    // Access the fields of a tuple struct
-    println!("pair contains {:?} and {:?}", pair.0, pair.1);
-
-    // Destructure a tuple struct
-    let Pair(integer, decimal) = pair;
-
-    println!("pair contains {:?} and {:?}", integer, decimal);
-
-    let area = rect_area(&_rectangle);
-    println!("area of rectangle {}", area);
 
 }
