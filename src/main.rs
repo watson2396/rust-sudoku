c02ced0974992e81dd8852249e435b3d1f1949ef
jwatson <jwatson2396@gmail.com>
2025-12-10 20:23:37 -0500

    added block struct; working on print impl

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 use rand::prelude::*;
 
 // lets try filling an array with any missing numbers from 1-9
@@ -16,24 +15,51 @@ use rand::prelude::*;
     a  b  c  d  e  f  h  i  j
 */
 
+struct Block {
+    grid: [[i8; 3]; 3]
+}
+
+impl Block {
+    pub fn new() -> Block {
+        Block { grid: [[0i8; 3]; 3] }
+    }
+
+    pub fn print(self) {
+        println!("|------------------------------|");
+        for i in 0..self.grid.len() {
+            let mut s: String = String::from("| ");
+            for j in 0..self.grid[i].len() {
+               let a = self.grid[i][j].to_string();
+               s = s.to_owned() + &a + " |"; 
+            }
+            println!("{}", s);
+        }
+        println!("|------------------------------|");
+    }
+}
+
+// struct Board {
+//     grid: [[Block; 3]; 3]
+// }
+
+
 // here in the rust by example site, https://doc.rust-lang.org/rust-by-example/flow_control/while.html
 fn main() {
 
     println!("Hello, World!");
 
-    // Fixed-size array (type signature is superfluous).
-    let mut a: [i8; 9];
+    let mut block: Block = Block::new();
 
     // Get an RNG:
     let mut rng = rand::rng();
 
-    println!("i8: '{}'", rng.random_range(1..10));
-    let mut x = 0;
-    while x < 10 {
-        a[x] = rng.random_range(1..10);
-
-        x += 1;
+    // fill in Block randomly
+    for i in 0..block.grid.len() {
+        for j in 0..block.grid[i].len() {
+            block.grid[i][j] = rng.random_range(1..10);
+            //println!("x-index: {}, y-index: {}, value: {}", i, j, block.grid[i][j]);
+        }
     }
 
-
+    block.print();
 }
